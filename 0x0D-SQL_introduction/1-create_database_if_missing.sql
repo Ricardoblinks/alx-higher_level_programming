f64fbb80cd91c9e14b16f1d8d31f43e8a2704014
Richard James <ricardoblinks1@gmail.com>
2023-02-15 01:14:45 +0100

    commit for sql

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
--- create a new database hbtn_0c_0 if it doesn't already exists
-CREATE DATABASE IF NOT EXISTS "hbtn_0c_0";
+-- Creates the database hbtn_0c_0.
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
